d8ca9e90e77b99adee6e7f17c5517150dc97b033
nasheya <nasheya.rahman@gmail.com>
2016-04-08 17:30:14 -0500

    Added timeline and added some attempts for rotating background images

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,34 @@
+// function cycleImages(){
+//       var $active = $('#background_cycler .active');
+//       var $next = ($('#background_cycler .active').next().length > 0) ? $('#background_cycler .active').next() : $('#background_cycler img:first');
+//       $next.css('z-index',2);//move the next image up the pile
+//     $active.fadeOut(1500,function(){//fade out the top image
+//     $active.css('z-index',1).show().removeClass('active');//reset the z-index and unhide the image
+//       $next.css('z-index',3).addClass('active');//make the next image the top one
+//       });
+//     }
+
+//     $(window).load(function(){
+//     $('#background_cycler').fadeIn(1500);//fade the background back in once all the images are loaded
+//       // run every 7s
+//       setInterval('cycleImages()', 7000);
+//     })
+
 $(document).ready(function () {
+  // Cycler={};
+  // Cycler.src=['../img/header.jpg', '../img/logo/LCGlogo.png', '../img/people/NasheyaRahman.png'];
+  // Cycler.cur=0;
+  // Cycler.cycle=function() {
+  //   if(++Cycler.cur>=Cycler.src.length) {
+  //     Cycler.cur=0;
+  //   }
+
+  //   $('header').css('background-image', 'url('+ Cycler.src[Cycler.cur] +')');
+  //   setTimeout(Cycler.cycle, 5000);//5 seconds
+  // }
+
+  // Cycler.cycle();
+  
     $('.single-item').slick({
         infinite: false,
         speed: 300,
@@ -31,4 +61,9 @@ $(document).ready(function () {
         }
       ]
     });
+
+  
 });
+
+
+
